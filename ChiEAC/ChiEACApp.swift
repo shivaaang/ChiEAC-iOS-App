06d9b715de0c50f5e1f93aeaf82ba302537558c2
Shivaang <shivaang@bu.edu>
2025-08-09 10:11:10 -0400

    Initial Commit

---
+++ ChiEAC/ChiEACApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChiEACApp.swift
+//  ChiEAC
+//
+//  Created by Shivaang Kumar on 8/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ChiEACApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChiEACTests/ChiEACTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChiEACTests.swift
+//  ChiEACTests
+//
+//  Created by Shivaang Kumar on 8/9/25.
+//
+
+import Testing
+@testable import ChiEAC
+
+struct ChiEACTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
